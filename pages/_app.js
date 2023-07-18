1655f1d36505410d2491ac615d6d1f77d140f2e3
Shashwat Kapoor <114916282+shashwatDaoLens@users.noreply.github.com>
2023-07-18 12:55:19 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,26 @@
 import '@/styles/globals.css'
 import { SessionProvider } from "next-auth/react"
-
+import {useEffect} from 'react'
+import Script from "next/script";
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
+  useEffect(() => {
+    if (window.magicpulseinit) {
+      try {
+        window.magicpulseinit("YOUR_APP_ID");
+      } catch (error) {
+        console.error("Failed to load the Magic Pulse package", error);
+      }
+    }
+  }, []);
   return (
     <SessionProvider session={session}>
+     <Script
+        src="https://cdn.jsdelivr.net/npm/crunchit@latest/crunchit.min.js"
+        strategy="beforeInteractive"
+      />
       <Component {...pageProps} />
     </SessionProvider>
   )
